42b5658e35fe2a77398779f8d78375023430edcf
Cat Marsh <c@catamarsh.com>
2020-03-21 03:16:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function shout(string) {
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase)
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase()
+  console.log(string.toUpperCase)
 }
 
 function logWhisper(string) {
