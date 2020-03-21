985cda054b9887b3d0abf2de119fb5de69666276
Cat Marsh <c@catamarsh.com>
2020-03-21 03:13:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase)
 }
 
-function sayhitoGrandma(string) {
+function sayHiToGrandma(string) {
     if (string.toLowerCase() === string)
     return "I can't hear you!";
     if (string.toUpperCase() === string)
