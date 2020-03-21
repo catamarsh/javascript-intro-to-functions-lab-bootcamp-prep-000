6fef612e3ef2a137a17d58adf2893e0ae834cd5f
Cat Marsh <c@catamarsh.com>
2020-03-21 02:55:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logShout(string) {
 }
 
 function logWhisper(string) {
-  console.log.toLowerCase()
+  console.log()
 }
 function sayhitoGrandma(string) {
     if (string.toLowerCase() === string){
